bb984a88bec27d672a1624c1f6f987fe08855dc8
Amin Ariana <aminka@gmail.com>
2013-11-09 21:58:03 -0800

    Reflect Title and Callback in response

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,7 +4,8 @@ class MainController < ApplicationController
 
   def map
     @errors = []
-    @errors << "Missing callback URL param 'callback'." unless params[:callback]
+    @errors << "Missing callback URL param 'callback'." unless @callback = params[:callback]
+    @errors << "Missing Page Title param 'title'." unless @title = params[:title]
     render :error unless @errors.empty?
   end
 end
