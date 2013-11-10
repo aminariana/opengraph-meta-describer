89980108c1e1dd7836be6f9a2fbf9d047fe2545d
Amin Ariana <aminka@gmail.com>
2013-11-09 22:24:21 -0800

    Validate URL regrex

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -4,9 +4,10 @@ class MainController < ApplicationController
 
   def map
     @errors = []
-    @errors << "Missing Callback URL param 'callback'." unless @callback = params[:callback]
     @errors << "Missing Page Title param 'title'." unless @title = params[:title]
     @errors << "Missing Page Description param 'description'." unless @description = params[:description]
+    @errors << "Missing Callback URL param 'callback'." unless @callback = params[:callback]
+    @errors << "Callback URL '#{@callback}' is not a valid absolute URL." unless @callback and @callback =~ /^#{URI::ABS_URI}$/
     render :error unless @errors.empty?
   end
 end
