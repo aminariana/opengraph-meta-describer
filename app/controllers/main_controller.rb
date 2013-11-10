66ab4cdd388013461b427ca74955c0d32abbc6b2
Amin Ariana <aminka@gmail.com>
2013-11-09 20:51:53 -0800

    Add a landing page

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,4 +1,7 @@
 class MainController < ApplicationController
+  def landing
+  end
+
   def map
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 OpengraphMetaDescriber::Application.routes.draw do
-  root :to => "main#map"
+  root :to => "main#landing"
+
+  get "/map", :to => "main#map"
 end
 #== Route Map
-# Generated on 09 Nov 2013 20:47
+# Generated on 09 Nov 2013 20:50
 #
-# root GET / main#map
+# root GET /              main#landing
+#  map GET /map(.:format) main#map
