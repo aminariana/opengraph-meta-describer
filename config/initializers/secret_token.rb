7b139e9c6b41469b8b44e91fa6cb7932459a0c46
Amin Ariana <aminka@gmail.com>
2013-11-09 20:21:39 -0800

    Rails new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+OpengraphMetaDescriber::Application.config.secret_key_base = '7270df3a1b172eb5f36b4189f4d49b9c2db242b46aeec56bbcabea3fda54e1096e1f0646f23c2cb07095e67499e8818e696f4018510d93a98b1d41085855f5f4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+OpengraphMetaDescriber::Application.config.session_store :cookie_store, key: '_opengraph-meta-describer_session'
