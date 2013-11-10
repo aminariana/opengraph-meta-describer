a2a9a3e6c6ace2ea173a836c77e5e3d0352bd49d
Amin Ariana <aminka@gmail.com>
2013-11-09 22:38:27 -0800

    Add og:image meta tag

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -5,10 +5,12 @@ class MainController < ApplicationController
   def map
     @errors = []
     @errors << "Missing Page Title param 'title'." unless @title = params[:title]
-    @errors << "Missing Site Name param 'site_name'." unless @site_name = params[:site_name]
     @errors << "Missing Page Description param 'description'." unless @description = params[:description]
+    @errors << "Missing Site Name param 'site_name'." unless @site_name = params[:site_name]
+    @errors << "Missing Image param 'image'." unless @image = params[:image]
     @errors << "Missing Callback URL param 'callback'." unless @callback = params[:callback]
-    @errors << "Callback URL '#{@callback}' is not a valid absolute URL." unless @callback and @callback =~ /^#{URI::ABS_URI}$/
+    @errors << "Image URL '#{@image}' is not a valid absolute URL." unless @image =~ /^#{URI::ABS_URI}$/
+    @errors << "Callback URL '#{@callback}' is not a valid absolute URL." unless @callback =~ /^#{URI::ABS_URI}$/
     render :error unless @errors.empty?
   end
 end
